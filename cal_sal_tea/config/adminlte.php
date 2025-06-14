f11f41d260d5df085bf2cedac335704a49afad0d
Pham Hoang Anh <117793920+vjintageboy@users.noreply.github.com>
2025-06-14 15:23:04 +0700

    13

--- cal_sal_tea/config/adminlte.php
+++ cal_sal_tea/config/adminlte.php
@@ -301,11 +301,11 @@ return [
     'menu' => [
 
     // Navbar items
-    [
-        'type' => 'navbar-search',
-        'text' => 'search',
-        'topnav_right' => true,
-    ],
+    // [
+    //     'type' => 'navbar-search',
+    //     'text' => 'search',
+    //     'topnav_right' => true,
+    // ],
     [
         'type' => 'fullscreen-widget',
         'topnav_right' => true,
